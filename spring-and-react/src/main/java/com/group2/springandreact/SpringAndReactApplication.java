6d8b24ebe107eff504583a7f0733097cc35a1d6f
Ryan Torrecampo <ryantorrecampo@Ryans-MacBook-Pro.local>
2020-04-07 20:28:11 -0500

    deployed on heroku

---
+++ spring-and-react/src/main/java/com/group2/springandreact/SpringAndReactApplication.java
@@ -0,0 +1,13 @@
+package com.group2.springandreact;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAndReactApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAndReactApplication.class, args);
+	}
+
+}
